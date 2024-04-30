b3859bb1be8b275f1995c398e3c7bb1509c53f85
ElGhazi1 <mohamedelghazi704@gmail.com>
2024-04-30 16:05:23 +0100

    New Changes

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ const router = express.Router();
 
 const  app = express();
 
+app.use(router);
+
+router.get('/' , (req , res)=>{
+    res.send({
+        message:'Hello World!'
+    });
+
+});
+
+app.listen( 3000, ()=> {
+        console.log('Server is ready');
+} );
